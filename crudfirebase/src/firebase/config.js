2c20a8ffd95ed1bc70ad22cfc2fec95cb9e31dde
TikaYulian27 <tikayulian27@gmail.com>
2020-04-30 16:21:04 +0700

    set firebase config

---
+++ crudfirebase/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUIbUJjOL9TCJCpMpUOcqT59C8qlHXmsk",
+    authDomain: "fir-login-cde0c.firebaseapp.com",
+    databaseURL: "https://fir-login-cde0c.firebaseio.com",
+    projectId: "fir-login-cde0c",
+    storageBucket: "fir-login-cde0c.appspot.com",
+    messagingSenderId: "852182476606",
+    appId: "1:852182476606:web:23fa096f0eff9077ee6e6f",
+    measurementId: "G-BSHDFKPJ7Z"
+  };
+
+  export default firebaseConfig;
